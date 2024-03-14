bb080da2d5e6efb895072d6285002dc54679be83
HSL-GliTcheur <hsl.glitcheur@gmail.com>
2024-03-14 16:12:43 +0100

    modification des fichier de base bootstrape pour changer le bg en orange et debut de la partie mes inspirations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+
+console.log($(window).scroll());
+
+var iScrollPos = 0;
+$(window).scroll(function () {
+    var iCurScrollPos = $(this).scrollTop();
+    if (iCurScrollPos > iScrollPos) {
+        $('#backToTop').removeClass('show');
+    } else {
+        $('#backToTop').addClass('show');
+    }
+    iScrollPos = iCurScrollPos;
+});
+

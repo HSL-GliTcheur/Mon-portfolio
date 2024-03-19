6a8898ae84750e8bb20b62186d38dc64e100b744
HSL-GliTcheur <hsl.glitcheur@gmail.com>
2024-03-19 10:49:51 +0100

    debut de php

--- index.php
+++ index.php
@@ -729,3 +729,13 @@
 </body>
 
 </html>
+
+<?php
+try {
+    // On se connecte à MySQL
+    $mysqlClient = new PDO('mysql:host=localhost:3306;dbname=admin_cc;charset=utf8', 'xxtofoo_admin', '#admin2024');
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die ('Erreur : ' . $e->getMessage());
+}
+?>

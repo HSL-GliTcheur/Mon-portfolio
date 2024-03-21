bcfb45745ef4feb9767afb2f0cfa435f669563d4
HSL-GliTcheur <hsl.glitcheur@gmail.com>
2024-03-21 15:02:43 +0100

    page admin bug corrigé

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,18 @@
 <?php session_start();
+
+try {
+    // On se connecte à MySQL
+    $mysqlClient = new PDO('mysql:host=localhost:3306;dbname=admin_cc;charset=utf8', 'xxtofoo_admin', '#admin2024');
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die ('Erreur : ' . $e->getMessage());
+}
+
 if ($_SESSION['logedin'] == false) {
     header('Location: ./login.php');
     exit;
+
+
 }
 ?>
 
--- admin/login.php
+++ admin/login.php
@@ -77,7 +77,7 @@
 
     try {
         // On se connecte à MySQL
-        $mysqlClient = new PDO('mysql:host=localhost;dbname=portfolio;charset=utf8', 'root', '');
+        $mysqlClient = new PDO('mysql:host=localhost:3306;dbname=admin_cc;charset=utf8', 'xxtofoo_admin', '#admin2024');
     } catch (Exception $e) {
         // En cas d'erreur, on affiche un message et on arrête tout
         die ('Erreur : ' . $e->getMessage());
@@ -121,10 +121,10 @@
                 <button type="submit" name="ok">Se connecter</button>
                 <?php if (isset ($message)) {
                     ?>
-                <p>
-                    <?php echo $message; ?>
-                </p>
-                <?php
+                    <p>
+                        <?php echo $message; ?>
+                    </p>
+                    <?php
                 } ?>
             </div>
 
